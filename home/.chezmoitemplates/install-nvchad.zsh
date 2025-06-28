49c9f1f4f105aa0795b16b0ea2bb19a05d2c0b28
Keane Watterson <keane.watterson@olilo.io>
2025-06-27 17:23:41 -0700

    Enable direct install of NvChad rather than chezmoiexternal

---
+++ home/.chezmoitemplates/install-nvchad.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+set -euo pipefail
+
+{{ template "init-env.sh" . }}
+{{ template "util-log.sh" . }}
+
+
+if ! [[ -d "${XDG_CONFIG_HOME}/nvim" ]]; then
+    log_info "Installing NvChad"
+    git clone https://github.com/NvChad/starter "${XDG_CONFIG_HOME}/nvim"
+else
+    log_info "NvChad is installed\n"
+fi
