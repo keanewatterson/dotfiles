ca3b09b7e466ec0aa7a2d1f7710c567fa5234185
Keane Watterson <keane.watterson@olilo.io>
2025-07-31 10:33:21 -0700

    Updated preferences

--- home/private_dot_config/private_nvim/private_lua/private_options.lua
+++ home/private_dot_config/private_nvim/private_lua/private_options.lua
@@ -2,6 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 vim.opt.colorcolumn = "80,100"
+vim.opt.clipboard = "unnamed,unnamedplus"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
