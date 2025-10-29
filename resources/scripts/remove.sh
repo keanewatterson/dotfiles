c5bcf71d4bccaa0b736f8534f5c9cb52949e3489
Keane Watterson <keane.watterson@olilo.io>
2025-10-28 20:40:52 -0700

    Updated preferences

--- resources/scripts/remove.sh
+++ resources/scripts/remove.sh
@@ -8,29 +8,42 @@ XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}"
 
 typeset -a path_list=(
-    ${XDG_CACHE_HOME}/nvim
-    ${XDG_CACHE_HOME}/oh-my-posh
-    ${XDG_CACHE_HOME}/uv
-    ${XDG_CONFIG_HOME}/chezmoi
-    ${XDG_CONFIG_HOME}/nvim
-    ${XDG_CONFIG_HOME}/oh-my-posh
-    ${XDG_CONFIG_HOME}/zsh
-    ${XDG_DATA_HOME}/chezmoi
-    ${XDG_DATA_HOME}/nvim
-    ${XDG_DATA_HOME}/mamba
-    ${XDG_DATA_HOME}/sdkman
-    ${XDG_DATA_HOME}/zoxide
-    ${XDG_STATE_HOME}/oh-my-posh
-    ${XDG_STATE_HOME}/nvim
-    ${HOME}/.local//bin/chezmoi
-    ${HOME}/.config/zsh
-    ${HOME}/.config/nvim
-    ${HOME}/.zshenv
+
     ${HOME}/.gitconfig
     ${HOME}/.gitignore_global
+    ${HOME}/.ssh
+
+    ${XDG_CACHE_HOME}/mise
+    ${XDG_CONFIG_HOME}/mise
+    ${XDG_DATA_HOME}/mise
+    ${XDG_STATE_HOME}/mise
+
+
+    ${XDG_CACHE_HOME}/nvim
+    ${XDG_CONFIG_HOME}/nvim
+    ${XDG_DATA_HOME}/nvim
+    ${XDG_STATE_HOME}/nvim
+
+    ${XDG_CACHE_HOME}/oh-my-posh
+    ${XDG_CONFIG_HOME}/oh-my-posh
+    ${XDG_STATE_HOME}/oh-my-posh
+
+    ${XDG_CONFIG_HOME}/chezmoi
+    ${XDG_DATA_HOME}/chezmoi
+    ${HOME}/.local//bin/chezmoi
+
+    ${XDG_DATA_HOME}/mamba
     ${HOME}/.conda
     ${HOME}/.mamba
-    ${HOME}/.ssh
+
+    ${XDG_CACHE_HOME}/uv
+    ${XDG_DATA_HOME}/mise
+    
+    ${XDG_DATA_HOME}/zoxide
+
+    ${XDG_CONFIG_HOME}/zsh
+    ${HOME}/.zshenv
+
 )
 
 
