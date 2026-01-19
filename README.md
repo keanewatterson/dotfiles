510e02ce4bd065bc19326420eeffa53baa3b094a
Keane Watterson <1178773+keanewatterson@users.noreply.github.com>
2026-01-18 17:30:26 -0800

    Updated preferences

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 Dotfiles and package installation for macOS and Ubuntu managed with [chezmoi](https://chezmoi.io).
 
-
+## Quick Start
 Install chezmoi using default installer and initialize via public repository.
 
 ```sh
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b "${HOME}/.local/bin" init --apply https://github.com/keanewatterson/dotfiles.git
+```
+
+## Development
+Evaluate template modifications based on chezmoi mode.
+```sh
+# initialization
+chezmoi init --source . --config /tmp//test-chezmoi-init.toml
+
+# general case
+chezmoi execute-template < test.tmpl > /tmp/test.txt
 ```
--- home/.chezmoitemplates/install-finalize.zsh
+++ home/.chezmoitemplates/install-finalize.zsh
@@ -5,22 +5,15 @@ set -euo pipefail
 {{ template "init-env.sh" . }}
 {{ template "util-log.sh" . }}
 
+# general case
 log_info "Configure chezmoi repository for ssh protocol\n"
-git -C "${XDG_DATA_HOME}/chezmoi" remote set-url origin git@github.com:keanewatterson/dotfiles.git
+git -C "${XDG_DATA_HOME}/chezmoi" remote set-url origin git@github.com:{{ .instance.user_github_account }}/dotfiles.git
 
 if ! [[ -f "{{ .chezmoi.sourceDir }}/.chezmoidata/instance-options.toml " ]]; then
     mkdir -p "{{ .chezmoi.sourceDir }}/.chezmoidata"
     cp "{{ .chezmoi.workingTree }}/resources/chezmoi/instance-options.toml" \
         "{{ .chezmoi.sourceDir }}/.chezmoidata/instance-options.toml"
 fi
-
-{{ if eq .chezmoi.os "darwin" -}}
-path_tmp="${HOME}/.local/bin/chezmoi"
-if [[ -f "/opt/homebrew/bin/chezmoi" && -f "${path_tmp}" ]]; then
-    log_info "Remove bootstrap executable: ${path_tmp}\n"
-    rm -f "${path_tmp}"
-fi
-{{- end }}
 
 paths_rm=("${HOME}/.zsh_history")
 
@@ -37,3 +30,29 @@ for path_rm in "${paths_rm[@]}"; do
 done
 
 mkdir -p "${HOME}/Projects"
+
+
+# linux specific
+{{ if eq .chezmoi.os "linux" -}}
+
+# fedora
+{{ if eq .chezmoi.osRelease.id "fedora" -}}
+
+{{ if lookPath "sshd" -}}
+log_info "Enable sshd\n"
+sudo systemctl enable --now sshd
+{{- end }}
+
+{{- end }}
+
+{{- end }}
+
+
+# macos specific
+{{ if eq .chezmoi.os "darwin" -}}
+path_tmp="${HOME}/.local/bin/chezmoi"
+if [[ -f "/opt/homebrew/bin/chezmoi" && -f "${path_tmp}" ]]; then
+    log_info "Remove bootstrap executable: ${path_tmp}\n"
+    rm -f "${path_tmp}"
+fi
+{{- end }}
